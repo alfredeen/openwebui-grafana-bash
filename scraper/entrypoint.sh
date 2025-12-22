35c62e35397780e17d3b03316e6143a87b47bb55
Johan Alfredéen <johan.alfredeen@gmail.com>
2025-12-22 15:26:23 +0100

    Add dryrun mode and fix influxdb write (#4)
    * Add an entrypoint loop for running the scraper script. Add a script to verify that InfluxDB is up and accessible.
    
    * Integrate scraper bash script.
    
    * Add yamllint config file.
    
    * Add dry_run mode, display running configuration settings, and fix an InfluxDB write error.

--- scraper/entrypoint.sh
+++ scraper/entrypoint.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 : "${SCRAPE_INTERVAL_SECONDS:=60}"
 
 # Help influx CLI find InfluxDB
-export INFLUX_HOST="${INFLUX_HOST:-${INFLUXDB_URL#http://}:${INFLUXDB_PORT:-8086}}"
 export INFLUX_URL="${INFLUX_URL:-${INFLUXDB_URL:-http://influxdb}:${INFLUXDB_PORT:-8086}}"
+export INFLUX_HOST="${INFLUX_HOST:-${INFLUX_URL}}"
 
 # state dir for persistent state of the lastrun timestamp
 mkdir -p /state
